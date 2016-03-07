d338a5d7cd3d9c25fc536831febf55a8425fb159
Kyle Yuasa <yuasakyl@uw.edu>
2016-03-06 21:06:12 -0800

    Form

---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<form action="go.php" method="post">
+Beer Lookup<input type="text" name="beer"><br>
+<input type="submit">
+</form>
+
+try {
+		$conn = new PDO('')
+		$
+}
+
+Result: 
+
+</body>
+</html>
