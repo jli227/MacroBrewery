083e8ab13d3b23e13eabeba81b05605628cb24f0
Kyle Yuasa <yuasakyl@uw.edu>
2016-03-06 21:30:11 -0800

    Changes to db connection

--- connect.php
+++ connect.php
@@ -2,34 +2,37 @@
 function connect() {
 		try {
 
-		$servername = "IS-HAY04.ischool.uw.edu";
-		$username = "info445";
-		$password = "GoHuskies!";
-		$dbname = "MacroBrewery2";
+			$servername = "IS-HAY04.ischool.uw.edu";
+			$username = "info445";
+			$password = "GoHuskies!";
+			$dbname = "MacroBrewery2";
 
 
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		}
-		$value = $_POST['bname'];
-		$conn = new PDO($servername, $username, $password, $dbname);
-		$stmt = $conn-> prepare("SELECT 'BeerName', 'BeerPrice', 'BeerVolume', 'BeerDescr' WHERE 'BeerName' LIKE '%$value%'");
+			// Check connection
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			}
+			$value = $_POST['bname'];
+			$conn = new PDO($servername, $username, $password, $dbname);
+			$stmt = $conn-> prepare("SELECT 'BeerName', 'BeerPrice', 'BeerVolume', 'BeerDescr' WHERE 'BeerName' LIKE '%{$value}%'");
+			$stmt->execute();
 
-		foreach ($conn->query($sql) as $row) {
-	        print $row['BeerName'] . "\t";
-	        print $row['BeerPrice'] . "\t";
-	        print $row['BeerVolume'] . "\t";
-	        print $row['BeerDescr'] . "\n";
-    	}
 
-		$stmt->close();
-	
-		$conn->close();
+
+			foreach ($conn->query($stmt) as $row) {
+		        print $row['BeerName'] . "\t";
+		        print $row['BeerPrice'] . "\t";
+		        print $row['BeerVolume'] . "\t";
+		        print $row['BeerDescr'] . "\n";
+	    	}
+
+			$stmt->close();
+		
+			$conn->close();
 		}
 
 		catch(PDOException $e) {
-		echo 'ERROR: ' . $e->getMessage();
+			echo 'ERROR: ' . $e->getMessage();
 		}
 	}
 
